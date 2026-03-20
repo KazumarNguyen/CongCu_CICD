6605c60ddb0281bdac54c4df03fe467d6c953eaf
KazumarNguyen <giatrieunp@gmail.com>
2026-03-20 15:52:47 +0700

    Initial commit - đưa code lên repo CICD

---
+++ src/main/java/com/example/giatrieu_3393/Giatrieu3393Application.java
@@ -0,0 +1,13 @@
+package com.example.giatrieu_3393;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Giatrieu3393Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Giatrieu3393Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/giatrieu_3393/Giatrieu3393ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giatrieu_3393;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Giatrieu3393ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
